b0bfb3a9b56bc97706ba20f5fa5bb391da5d0d19
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-03-30 22:52:43 +0800

    Add new font

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Titillium_Web } from "next/font/google";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 
@@ -7,6 +7,12 @@ const roboto_init = Roboto({
   subsets: ["latin"],
   weight: ["100", "300", "400", "500", "700", "900"],
   variable: "--font-roboto",
+});
+
+const titilliumWeb_init = Titillium_Web({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "600", "700", "900"],
+  variable: "--font-titillium",
 });
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,7 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={roboto_init.variable}>{children}</body>
+      <body className={`${roboto_init.variable} ${titilliumWeb_init.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
