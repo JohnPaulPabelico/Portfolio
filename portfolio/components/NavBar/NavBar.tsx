a7cf422a9b71f60ed5d877f525d1af95ce77c6d5
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-03-31 15:43:20 +0800

    Revamped Nav Bar

--- portfolio/components/NavBar/NavBar.tsx
+++ portfolio/components/NavBar/NavBar.tsx
@@ -1,60 +1,110 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { BsMoonStars } from "react-icons/bs";
+import { FaBars } from "react-icons/fa";
+import { GrClose } from "react-icons/gr";
 
 function NavBar() {
-  return (
-    <div>
-      {/* ------- NAVIGATON BAR ------- */}
-      <nav className="lg:fixed absolute w-full shadow px-5 flex justify-between bg-white z-10">
-        <div>
-          {" "}
-          <Image
-            src="/Jp Black.png"
-            alt="Icon"
-            width={80}
-            height={80}
-            className="rounded-full"
-          />
-        </div>
+  const [barState, setBarState] = useState(false);
 
-        <ul className="flex items-center">
-          {/* <li className="text-xl cursor-pointer">
-              <BsFillMoonStarsFill />
-            </li> */}
-          <li>
+  return (
+    <div className="flex items-center lg:fixed fixed top-0 w-full shadow px-5 min-h-30 justify-between bg-white z-10">
+      <div>
+        {" "}
+        <Image
+          src="/Jp Black.png"
+          alt="Icon"
+          width={80}
+          height={80}
+          className="rounded-full"
+        />
+      </div>
+
+      <div
+        className={`transition ease-in lg:ml-auto lg:static absolute  lg:min-h-fit min-h-screen lg:w-auto w-full top-[100%] left-0 py-10 lg:py-0 ${
+          barState ? "bg-white" : "bg-transparent"
+        }`}
+      >
+        <ul
+          className={`flex-col lg:flex-row flex items-center gap-16 lg:gap-0 `}
+        >
+          <li className={`${barState ? "block" : "hidden lg:block"}`}>
             <a
-              className="ml-8 text-xl text-black titillium font-bold rounded-full m-2 hover:text-blue-500 transition duration-200"
+              className="lg:ml-8 lg:text-xl text-3xl text-black titillium font-bold rounded-full m-2 hover:text-blue-500 transition duration-200 "
               href="#home"
+              onClick={() => {
+                setBarState(false);
+                console.log(barState);
+              }}
             >
               Home
             </a>
           </li>
-          <li>
+          <li className={`${barState ? "block" : "hidden lg:block"}`}>
             <a
-              className="text-xl text-black titillium font-bold rounded-full m-2 hover:text-blue-500 transition duration-200"
+              className="lg:text-xl text-3xl text-black titillium font-bold rounded-full m-2 hover:text-blue-500 transition duration-200 "
               href="#about"
+              onClick={() => {
+                setBarState(false);
+                console.log(barState);
+              }}
             >
               About
             </a>
           </li>
-          <li>
+          <li className={`${barState ? "block" : "hidden lg:block"}`}>
             <a
-              className="text-xl text-black titillium font-bold rounded-full m-2 hover:text-blue-500 transition duration-200"
+              className="lg:text-xl text-3xl text-black titillium font-bold rounded-full m-2 hover:text-blue-500 transition duration-200  "
               href="#projects"
+              onClick={() => {
+                setBarState(false);
+                console.log(barState);
+              }}
             >
               Projects
             </a>
           </li>
-          <li>
+          <li className={`${barState ? "block" : "hidden lg:block"}`}>
             <a
-              className="text-xl text-black titillium font-bold rounded-full m-2 hover:text-blue-500 transition duration-200"
+              className="lg:text-xl text-3xl text-black titillium font-bold rounded-full m-2 hover:text-blue-500 transition duration-200  "
               href="#contact"
+              onClick={() => {
+                setBarState(false);
+                console.log(barState);
+              }}
             >
               Contact
             </a>
           </li>
         </ul>
-      </nav>
+      </div>
+
+      <div className="">
+        <ul className="flex items-center">
+          {" "}
+          <li className="m-3 text-2xl cursor-pointer hover:text-blue-500 transition duration-200">
+            <BsMoonStars />
+          </li>
+          <li className="m-3 text-2xl cursor-pointer hover:text-blue-500 transition duration-200 lg:hidden">
+            {barState ? (
+              <GrClose
+                onClick={() => {
+                  setBarState(!barState);
+                  console.log(barState);
+                }}
+              />
+            ) : (
+              <FaBars
+                onClick={() => {
+                  setBarState(!barState);
+                  console.log(barState);
+                }}
+              />
+            )}
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
