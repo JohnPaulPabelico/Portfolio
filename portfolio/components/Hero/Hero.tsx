5d2acbc6d311d8f925d4ef9f55bb2f6645be9fd3
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-03-31 15:43:36 +0800

    idk

--- portfolio/components/Hero/Hero.tsx
+++ portfolio/components/Hero/Hero.tsx
@@ -5,10 +5,10 @@ import { IoIosContact } from "react-icons/io";
 
 function Hero() {
   return (
-    <div>
+    <div className="">
       <section id="home" className="bg-gray-100 min-h-dvh">
         <div className="lg:flex items-center justify-center lg:justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 lg:px-10 py-5 w-3/4">
-          <div className="mr-8 flex justify-center">
+          <div className="relative mr-8 flex justify-center">
             <Image
               src="/Jp.jpg"
               alt="Profile Pic"
