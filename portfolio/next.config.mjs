14dc14bab51a04f69e6bac4e1291bc192e7d86c1
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-03-31 13:35:44 +0800

    Add skillicons to Next Images

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "skillicons.dev",
+        port: "",
+        pathname: "/icons**",
+      },
+    ],
+    dangerouslyAllowSVG: true,
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
