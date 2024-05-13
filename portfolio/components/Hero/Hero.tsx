7506d0a4fb1cee7970286d8c514a119fbe7a1073
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-05-14 01:58:43 +0800

    Workaround

--- portfolio/components/Hero/Hero.tsx
+++ portfolio/components/Hero/Hero.tsx
@@ -22,40 +22,35 @@ function Hero() {
         <div className="mt-8 lg:mt-0 lg:flex items-center justify-center lg:justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 lg:px-10 py-5 w-3/4">
           <div className="relative mr-8 flex justify-center">
             {/* ---------- IMAGE ---------- */}
-            <Reveal>
-              <div className="heroblob"></div>
-            </Reveal>
+            <div className="heroblob"></div>
           </div>
 
           <div>
-            <Reveal>
-              <div className="text-center lg:text-start lg:text-7xl text-5xl font-bold titillium flex lg:justify-start justify-center tracking-widest items-center">
-                JP PABELICO{" "}
-              </div>
-            </Reveal>
-            <Reveal>
-              <h2
-                className={`lg:text-start text-center mt-1 tracking-widest text-2xl font-bold titillium ${
+            <div className="text-center lg:text-start lg:text-7xl text-5xl font-bold titillium flex lg:justify-start justify-center tracking-widest items-center">
+              JP PABELICO{" "}
+            </div>
+
+            <div className="flex justify-center lg:justify-start">
+              <div
+                className={`lg:text-start text-center mt-1 flex lg:justify-start justify-center tracking-widest text-2xl font-bold titillium ${
                   darkMode ? " text-gray-300" : " text-gray-700"
                 }`}
               >
                 Web Developer
-              </h2>
-            </Reveal>
+              </div>
+            </div>
 
             <div className="lg:max-w-lg ">
-              <Reveal>
-                <h1
-                  className={`lg:text-start text-center mt-3 text-light roboto ${
-                    darkMode ? " text-gray-300" : " text-gray-700"
-                  }`}
-                >
-                  Hey there, I&apos;m John Paul Pabelico, your go-to guru for
-                  all things Front-end and React! Hailing from the lively
-                  streets of the Philippines, I bring not just expertise, but a
-                  genuine passion for crafting captivating digital experiences.
-                </h1>
-              </Reveal>
+              <h1
+                className={`lg:text-start text-center mt-3 text-light roboto ${
+                  darkMode ? " text-gray-300" : " text-gray-700"
+                }`}
+              >
+                Hey there, I&apos;m John Paul Pabelico, your go-to guru for all
+                things Front-end and React! Hailing from the lively streets of
+                the Philippines, I bring not just expertise, but a genuine
+                passion for crafting captivating digital experiences.
+              </h1>
             </div>
             <span className="flex justify-center lg:justify-start text-4xl mt-8">
               <Link
@@ -64,9 +59,7 @@ function Hero() {
                 rel="noopener noreferrer"
                 className="hover:text-blue-500 transition duration-300"
               >
-                <Reveal>
-                  <IoLogoGithub />
-                </Reveal>
+                <IoLogoGithub />
               </Link>
               <Link
                 href="https://www.linkedin.com/in/jp-pabelico/"
@@ -74,17 +67,13 @@ function Hero() {
                 rel="noopener noreferrer"
                 className="hover:text-blue-500 transition duration-300 lg:ml-5 ml-3"
               >
-                <Reveal>
-                  <FaLinkedin />
-                </Reveal>
+                <FaLinkedin />
               </Link>
               <Link
                 href="#contact"
                 className="hover:text-blue-500 transition duration-300 lg:ml-5 ml-3"
               >
-                <Reveal>
-                  <IoIosContact />
-                </Reveal>
+                <IoIosContact />
               </Link>
             </span>
           </div>
