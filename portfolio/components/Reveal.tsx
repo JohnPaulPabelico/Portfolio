52b4631131c50fe3f2d6cde0ae109f0f3fba7d28
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-05-14 00:52:58 +0800

    Added a slide animation that I might use

--- portfolio/components/Reveal.tsx
+++ portfolio/components/Reveal.tsx
@@ -12,10 +12,12 @@ export const Reveal = ({ children, width = "fit-content" }: props) => {
   const isInView = useInView(ref, { once: true });
 
   const mainControls = useAnimation();
+  const slideControls = useAnimation();
 
   useEffect(() => {
     if (isInView) {
       mainControls.start("visible");
+      slideControls.start("visible");
     }
   }, [isInView]);
 
@@ -32,6 +34,24 @@ export const Reveal = ({ children, width = "fit-content" }: props) => {
       >
         {children}
       </motion.div>
+      {/* <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          backgroundColor: "blue",
+          zIndex: 20,
+        }}
+      /> */}
     </div>
   );
 };
